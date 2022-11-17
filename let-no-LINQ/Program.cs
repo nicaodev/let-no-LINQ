60317b23962e647e2f195c64d2923f143cac579d
nicaodev <alexandre.nicos@gmail.com>
2022-11-17 19:30:06 -0300

    init

---
+++ let-no-LINQ/Program.cs
@@ -0,0 +1,38 @@
+﻿using let_no_LINQ.models;
+
+
+var orders = new List<Order>
+{
+    new("20 itens", GeraItens(20)),
+    new("15 itens ", GeraItens(15)),
+    new("60 itens", GeraItens(60)),
+    new("50 itens", GeraItens(50))
+};
+
+List<OrderLine> GeraItens(int quantity)
+{
+    var result = new List<OrderLine>();
+    for (var i = 0; i < quantity; i++)
+        result.Add(new OrderLine($"Produto gerado: {i}", i, i));
+
+    return result;
+}
+
+
+var data =
+    from order in orders
+    let totalItens = order.OrderLines.Count
+    let total = order.OrderLines.Sum(x => x.preco)
+    where
+        totalItens > 10 &&
+        total > 100
+    select new
+    {
+        order.numero,
+        total,
+        totalItens
+    };
+
+
+foreach (var item in data)
+    Console.WriteLine($"Numero: {item.numero}, Total: {item.total}");
---
+++ let-no-LINQ/models/order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace let_no_LINQ.models
+{
+    public record Order(string numero, List<OrderLine> OrderLines);
+
+    public record OrderLine(string produto, int quantidade, decimal preco);
+}
